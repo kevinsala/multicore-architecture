62596994cdfd5337cac644ef364e3a1caa370335
Kevin Sala <kevin.sala@bsc.es>
2016-12-06 16:08:29 +0100

    Data Cache implemented and tested
    This data cache version does not support byte accesses.

--- cache_data.vhd
+++ cache_data.vhd
@@ -38,17 +38,6 @@ ARCHITECTURE cache_data_behavior OF cache_data IS
 
 	TYPE hit_t IS ARRAY(CACHE_LINES-1 DOWNTO 0) OF STD_LOGIC;
 
-	COMPONENT mux4_32bits
-		PORT(
-			input0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-			input1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-			input2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-			input3 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-			ctrl   : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-			output : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
-		);
-	END COMPONENT;
-
 	-- Determine the state of the cache:
 	-- READY: The cache will be ready to execute a new instruction in the next cycle.
 	-- LINEREQ: The cache is waiting to get a memory line from the memory. The cache
@@ -64,37 +53,42 @@ ARCHITECTURE cache_data_behavior OF cache_data IS
 	SIGNAL tag_fields   : tag_fields_t;
 	SIGNAL data_fields  : data_fields_t;
 
-	-- Determines whether the access has hit, which line and its data
-	SIGNAL hit : STD_LOGIC;
-	SIGNAL hit_line : hit_t;
-	SIGNAL hit_line_id : INTEGER RANGE 0 TO 3 := 0;
-	SIGNAL hit_line_data : STD_LOGIC_VECTOR(DATA_BITS-1 DOWNTO 0);
+	-- Determine whether the access has hit, which line and its data
+	SIGNAL hit : STD_LOGIC := '1';
 
-	-- Determine if it needs a replacement and which line is the least recently used
-	SIGNAL replacement : STD_LOGIC;
-	SIGNAL lru_line_id : INTEGER RANGE 0 TO 3;
+	-- Determine the target line of the access
+	SIGNAL target_line : INTEGER RANGE 0 TO 3 := 0;
 
-	-- Determine the desired word of the line
-	SIGNAL requested_word : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	-- Determine the target word of the line
+	SIGNAL target_word : INTEGER RANGE 0 TO 3 := 0;
 
-	-- Procedure to reset the cache
+
+	-- Procedure to reset and initialize the cache
 	PROCEDURE reset_cache(
-			SIGNAL state : INOUT state_t;
-			SIGNAL done : OUT STD_LOGIC;
-			SIGNAL lru_fields : INOUT lru_fields_t;
-			SIGNAL valid_fields : INOUT valid_fields_t ) IS
+			SIGNAL state : OUT state_t;
+			SIGNAL lru_fields : OUT lru_fields_t;
+			SIGNAL valid_fields : OUT valid_fields_t;
+			SIGNAL dirty_fields : OUT dirty_fields_t;
+			SIGNAL mem_req : OUT STD_LOGIC;
+			SIGNAL mem_we  : OUT STD_LOGIC
+		) IS
 	BEGIN
 		-- Initialize LRU and valid fields
 		FOR i IN 0 TO CACHE_LINES-1 LOOP
 			lru_fields(i) <= i;
 			valid_fields(i) <= '0';
+			dirty_fields(i) <= '0';
 		END LOOP;
 
 		-- Set ready the cache
 		state <= READY;
-		done <= '1';
+
+		-- Cancel any memory request
+		mem_req <= '0';
+		mem_we <= '0';
 	END PROCEDURE;
 
+	-- Procedure to execute the Least Recently Used alogrithm
 	PROCEDURE LRU_execute(
 			SIGNAL lru_fields : INOUT lru_fields_t;
 			VARIABLE line_id : IN INTEGER RANGE 0 TO 3
@@ -108,104 +102,155 @@ ARCHITECTURE cache_data_behavior OF cache_data IS
 		lru_fields(line_id) <= 0;
 		END LOOP;
 	END PROCEDURE;
+
+	PROCEDURE check_hit(
+			SIGNAL addr : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			SIGNAL valid_fields : IN valid_fields_t;
+			SIGNAL tag_fields : IN tag_fields_t;
+			VARIABLE hit : OUT STD_LOGIC;
+			VARIABLE hit_line_id : OUT INTEGER RANGE 0 TO 3
+		) IS
+		VARIABLE hit_line : hit_t;
+	BEGIN
+		FOR i IN 0 TO CACHE_LINES-1 LOOP
+			hit_line(i) := valid_fields(i) AND to_std_logic(tag_fields(i) = addr(31 DOWNTO 4));
+		END LOOP;
+
+		hit := hit_line(0) OR hit_line(1) OR hit_line(2) OR hit_line(3);
+
+		IF hit_line(0) = '1' THEN hit_line_id := 0;
+		ELSIF hit_line(1) = '1' THEN hit_line_id := 1;
+		ELSIF hit_line(2) = '1' THEN hit_line_id := 2;
+		ELSE hit_line_id := 3;
+		END IF;
+	END PROCEDURE;
+
+	PROCEDURE check_replacement(
+			SIGNAL lru_fields : IN lru_fields_t;
+			SIGNAL valid_fields : IN valid_fields_t;
+			SIGNAL dirty_fields : IN dirty_fields_t;
+			VARIABLE hit : IN STD_LOGIC;
+			VARIABLE replacement : OUT STD_LOGIC;
+			VARIABLE lru_line_id : INOUT INTEGER RANGE 0 TO 3
+		) IS
+	BEGIN
+		-- Conditional assignment to know which line is the least recentrly used
+		IF lru_fields(0) = 3 THEN lru_line_id := 0;
+		ELSIF lru_fields(1) = 3 THEN lru_line_id := 1;
+		ELSIF lru_fields(2) = 3 THEN lru_line_id := 2;
+		ELSE lru_line_id := 3;
+		END IF;
+
+		-- Logic to determine if the cache needs a replacement
+		replacement := NOT hit AND dirty_fields(lru_line_id) AND valid_fields(0) AND valid_fields(1) AND valid_fields(2) AND valid_fields(3);
+	END PROCEDURE;
+
+	PROCEDURE get_target_word(
+			SIGNAL addr : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			VARIABLE word : OUT INTEGER RANGE 0 TO 3
+		) IS
+	BEGIN
+		CASE addr(3 DOWNTO 0) IS
+			WHEN x"0" => word := 0;
+			WHEN x"4" => word := 1;
+			WHEN x"8" => word := 2;
+			WHEN x"C" => word := 3;
+			WHEN OTHERS => word := 0;
+		END CASE;
+	END PROCEDURE;
 BEGIN
 
-stage : process(clk)
-	VARIABLE target_line_id : INTEGER;
-	VARIABLE word : INTEGER RANGE 0 TO 3;
+execution : PROCESS(clk)
+	VARIABLE hit_i : STD_LOGIC;
+	VARIABLE replacement_i : STD_LOGIC;
+	VARIABLE target_line_i : INTEGER RANGE 0 TO 3;
+	VARIABLE target_word_i : INTEGER RANGE 0 TO 3;
 	VARIABLE msb_pos : INTEGER RANGE 0 TO 127;
 	VARIABLE lsb_pos : INTEGER RANGE 0 TO 127;
+	VARIABLE serve_access : BOOLEAN;
 BEGIN
-	IF rising_edge(clk) AND reset = '0' THEN
-		IF state = READY THEN
-			IF hit = '1' THEN
-				target_line_id := hit_line_id;
-				IF we = '1' THEN
-					word := to_integer(unsigned(requested_word));
-					msb_pos := (word + 1) * WORD_BITS - 1;
-					lsb_pos := word * WORD_BITS;
-					data_fields(target_line_id)(msb_pos DOWNTO lsb_pos) <= data_in;
-					dirty_fields(target_line_id) <= '1';
-				END IF;
-				LRU_execute(lru_fields, target_line_id);
-				done <= '1';
-			ELSE
-				IF replacement = '1' THEN
-					state <= LINEREPL;
-					mem_addr <= tag_fields(lru_line_id) & "0000";
-					mem_data_out <= data_fields(lru_line_id);
-					mem_we <= '1';
+	serve_access := FALSE;
+
+	IF rising_edge(clk) THEN
+		IF reset = '0' THEN
+			target_line_i := target_line;
+			-- It's ready to process a new access
+			IF state = READY THEN
+				check_hit(addr, valid_fields, tag_fields, hit_i, target_line_i);
+
+				IF hit_i = '1' THEN
+					serve_access := TRUE;
 				ELSE
+					check_replacement(lru_fields, valid_fields, dirty_fields, hit_i, replacement_i, target_line_i);
+					IF replacement_i = '1' THEN
+						state <= LINEREPL;
+						mem_addr <= tag_fields(target_line_i) & "0000";
+						mem_data_out <= data_fields(target_line_i);
+						mem_we <= '1';
+					ELSE
+						state <= LINEREQ;
+						mem_addr <= addr;
+						mem_we <= '0';
+					END IF;
+					mem_req	<= '1';
+				END IF;
+
+				hit <= hit_i;
+				target_line <= target_line_i;
+
+			-- It's waiting for a confirmation of line replacement
+			ELSIF state = LINEREPL THEN
+				IF mem_done = '1' THEN
 					state <= LINEREQ;
 					mem_addr <= addr;
 					mem_we <= '0';
+					mem_req <= '1';
 				END IF;
-				mem_req	<= '1';
-				done <= '0';
+
+			-- It's waiting for a memory line
+			ELSIF state = LINEREQ THEN
+				IF mem_done = '1' THEN
+					state <= READY;
+					mem_req <= '0';
+					mem_we <= '0';
+					valid_fields(target_line_i) <= '1';
+					dirty_fields(target_line_i) <= '0';
+					tag_fields(target_line_i) <= addr(31 DOWNTO 4);
+					data_fields(target_line_i) <= mem_data_in;
+					serve_access := TRUE;
+				END IF;
 			END IF;
-		ELSIF state = LINEREPL THEN
-			IF mem_done = '1' THEN
-				state <= LINEREQ;
-				mem_addr <= addr;
-				mem_we <= '0';
-				mem_req <= '1';
+
+			-- It can serve the access
+			IF serve_access THEN
+				get_target_word(addr, target_word_i);
+
+				IF we = '1' THEN
+					msb_pos := (target_word_i + 1) * WORD_BITS - 1;
+					lsb_pos := target_word_i * WORD_BITS;
+
+					dirty_fields(target_line_i) <= '1';
+					data_fields(target_line_i)(msb_pos DOWNTO lsb_pos) <= data_in;
+				END IF;
+				LRU_execute(lru_fields, target_line_i);
+
+				hit <= '1';
+				target_word <= target_word_i;
 			END IF;
-		ELSIF state = LINEREQ THEN
-			target_line_id := lru_line_id;
-			IF mem_done = '1' THEN
-				state <= READY;
-				mem_req <= '0';
-				mem_we <= '0';
-				valid_fields(target_line_id) <= '1';
-				dirty_fields(target_line_id) <= '0';
-				tag_fields(target_line_id) <= addr(31 DOWNTO 4);
-				data_fields(target_line_id) <= mem_data_in;
-				LRU_execute(lru_fields, target_line_id);
-			END IF;
+		ELSE reset_cache(state, lru_fields, valid_fields, dirty_fields, mem_req, mem_we);
 		END IF;
-	ELSIF rising_edge(clk) AND reset = '0' THEN
-		reset_cache(state, done, lru_fields, valid_fields);
 	END IF;
-END PROCESS stage;
-
--- Logic to determine for each cache line if it's the requested line
-hit_line_loop : FOR i IN 0 TO 3 GENERATE
-	hit_line(i) <= valid_fields(i) AND to_std_logic(tag_fields(i) = addr(31 DOWNTO 4));
-END GENERATE;
-
--- Logic to determine if the access is a hit or a miss
-hit <= hit_line(0) OR hit_line(1) OR hit_line(2) OR hit_line(3);
-
--- Conditional assignment to know which line has hit
-hit_line_id <= 0 WHEN hit_line(0) = '1'
-				ELSE 1 WHEN hit_line(1) = '1'
-				ELSE 2 WHEN hit_line(2) = '1'
-				ELSE 3 WHEN hit_line(3) = '1'
-				ELSE 0;
-
--- Conditional assignment to know which line is the least recentrly used
-lru_line_id <= 0 WHEN lru_fields(0) = 3
-				ELSE 1 WHEN lru_fields(1) = 3
-				ELSE 2 WHEN lru_fields(2) = 3
-				ELSE 3 WHEN lru_fields(3) = 3
-				ELSE 0;
-
--- Logic to determine if the cache needs a replacement
-replacement <= NOT hit AND dirty_fields(lru_line_id) AND valid_fields(0) AND valid_fields(1) AND valid_fields(2) AND valid_fields(3);
-
--- Get the line which has hit
-hit_line_data <= data_fields(hit_line_id);
-
--- Determine the requested word of the access
-WITH addr(3 DOWNTO 0) SELECT requested_word <=
-		"00" WHEN x"0",
-		"01" WHEN x"4",
-		"10" WHEN x"8",
-		"11" WHEN x"C",
-		"00" WHEN OTHERS;
+END PROCESS execution;
 
 -- Set the data_out port to the requested word
-data_mux : mux4_32bits PORT MAP(hit_line_data(31 DOWNTO 0), hit_line_data(63 DOWNTO 32), hit_line_data(95 DOWNTO 64), hit_line_data(127 DOWNTO 96), requested_word, data_out);
+WITH target_word SELECT data_out <=
+		data_fields(target_line)(31  DOWNTO  0) WHEN 0,
+		data_fields(target_line)(63  DOWNTO 32) WHEN 1,
+		data_fields(target_line)(95  DOWNTO 64) WHEN 2,
+		data_fields(target_line)(127 DOWNTO 96) WHEN 3,
+		data_fields(target_line)(31  DOWNTO  0) WHEN OTHERS;
 
+-- The cache has completed an operation when the access is a hit
+done <= hit;
 
 END cache_data_behavior;
