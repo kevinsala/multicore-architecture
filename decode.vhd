d3d97e0e430341d8e18722bd63561c6bb6b2b67b
xarlieskin <carlos_calanda@hotmail.com>
2017-01-03 19:44:15 +0100

    bypasses

--- decode.vhd
+++ decode.vhd
@@ -83,13 +83,13 @@ BEGIN
 	jump <= to_std_logic(op_code_int = OP_JMP);
 	reg_src1_v <= NOT to_std_logic(op_code_int = OP_LI OR op_code_int = OP_NOP);
 	reg_src2_v <= to_std_logic(op_code_int = OP_ADD OR op_code_int = OP_SUB OR
-								op_code_int = OP_MUL OR op_code_int = OP_BEQ OR
-								op_code_int = OP_BNE OR op_code_int = OP_JMP OR
-								op_code_int = OP_STW OR op_code_int = OP_STB);
+							op_code_int = OP_MUL OR op_code_int = OP_BEQ OR
+							op_code_int = OP_BNE OR op_code_int = OP_JMP OR
+							op_code_int = OP_STW OR op_code_int = OP_STB);
 	inm_src2_v <= to_std_logic(op_code_int = OP_LI OR op_code_int = OP_STW OR
-	                            op_code_int = OP_STB OR op_code_int = OP_LDW OR
-	                            op_code_int = OP_LDB OR op_code_int = OP_BEQ OR
-	                            op_code_int = OP_BNE OR op_code_int = OP_JMP);
+							op_code_int = OP_STB OR op_code_int = OP_LDW OR
+							op_code_int = OP_LDB OR op_code_int = OP_BEQ OR
+							op_code_int = OP_BNE OR op_code_int = OP_JMP);
 	mul <= to_std_logic(op_code_int = OP_MUL);
 	mem_write <= to_std_logic(op_code_int = OP_STW OR op_code_int = OP_STB);
 	byte <= to_std_logic(op_code_int = OP_LDB OR op_code_int = OP_STB);
