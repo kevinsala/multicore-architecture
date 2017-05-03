d360c610b0b5809be0562038e2d0bbbb0884a718
Marc Marí <marc.maribarcelo@bsc.es>
2017-05-03 19:23:42 +0200

    Solve bug on detention unit
    When there was the sequence load (with miss) - load - mul (with dependence), the dependence provoked a reset of ALU stage, deleting the second load.

--- detention_unit.vhd
+++ detention_unit.vhd
@@ -60,7 +60,7 @@ ARCHITECTURE detention_unit_behavior OF detention_unit IS
 	SIGNAL conflict_MEM_dep : STD_LOGIC;
 	SIGNAL conflict_MEM : STD_LOGIC;
 BEGIN
-	conflict_ALU <= '1' WHEN mem_read_A = '1' AND ((reg_src1_D = reg_dest_A AND reg_src1_v_D = '1') OR (reg_src2_D = reg_dest_A AND reg_src2_v_D = '1' AND NOT mem_we_D = '1')) ELSE '0';
+	conflict_ALU <= '1' WHEN mem_read_A = '1' AND done_C = '1' AND ((reg_src1_D = reg_dest_A AND reg_src1_v_D = '1') OR (reg_src2_D = reg_dest_A AND reg_src2_v_D = '1' AND NOT mem_we_D = '1')) ELSE '0';
 	conflict_MUL_M1 <= '1' WHEN  mul_M1 = '1' AND ((reg_src1_D = reg_dest_A AND reg_src1_v_D = '1') OR (reg_src2_D = reg_dest_A AND reg_src2_v_D = '1')) ELSE '0';
 	conflict_MUL_M2 <= '1' WHEN  mul_M2 = '1' AND ((reg_src1_D = reg_dest_M2 AND reg_src1_v_D = '1') OR (reg_src2_D = reg_dest_M2 AND reg_src2_v_D = '1')) ELSE '0';
 	conflict_MUL_M3 <= '1' WHEN  mul_M3 = '1' AND ((reg_src1_D = reg_dest_M3 AND reg_src1_v_D = '1') OR (reg_src2_D = reg_dest_M3 AND reg_src2_v_D = '1' AND NOT mem_we_D = '1')) ELSE '0';
